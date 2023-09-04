12f94432fd4d0207fc1129a18f37660caddb62ab
YusupovAvazbek <yusupofavazbek@gmail.com>
2023-09-04 18:55:39 +0500

    medium clone

---
+++ src/main/java/com/example/mediumclone/MediumcloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.mediumclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediumcloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediumcloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mediumclone/MediumcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mediumclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediumcloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
